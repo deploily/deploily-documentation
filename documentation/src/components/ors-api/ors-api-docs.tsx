1cf0deabac9ce71a010a9c8510f40b86b8f62b90
ziadi kheira <kheira.ziadi@transformatek.dz>
2025-03-20 10:04:02 +0000

    Refactor API documentation structure and update links.

--- documentation/src/components/ors-api/ors-api-docs.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import OpenApiContainer from "../Api/openApiContainer";
-export default function OrsApiDocs() {
-    return (
-        <div title="Open route service API documentation">
-            <OpenApiContainer apiDescriptionUrl="./../openapi-files/ors-openapi.json" />
-        </div>
-    );
-}
