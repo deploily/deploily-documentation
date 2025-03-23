46612fc415e5578329e61290ba6fdee66ffaacbf
ziadi kheira <kheira.ziadi@transformatek.dz>
2025-03-23 13:32:11 +0000

    Create wilaya-commune page.

---
+++ documentation/src/pages/wilaya-commune-api-documentation-page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import OpenApiContainer from "../components/Api/openApiContainer";
+
+
+export default function page() {
+    return (
+        <div title="WilayaCommune API documentation">
+            <OpenApiContainer apiDescriptionUrl="./openapi-files/wilaya-commune-openapi.json" />
+        </div>
+    );
+}
